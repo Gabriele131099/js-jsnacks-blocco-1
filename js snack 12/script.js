8c72fb6fb00bc63a95b57237aaefc382701676a0
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-04 17:00:47 +0200

    struttura

--- js snack 12/script.js
+++ js snack 12/script.js
@@ -1,7 +1,6 @@
 var arrayName = ["Pippo" , "Paperino" , "Topolino", "Minnie" , "Pluto" , "Gabriele"]
-
 var arrayCognome = ["Sanna" , "Casu" , "Aguilera" , "Patermo" , "Riccioli" , "D'Agata"]
 var numInvitati = 3
-for (let i = 0; i < numInvitati; i++) {
+for (var i = 0; i < numInvitati; i++) {
     document.getElementById("stampa").innerHTML += "<li>" + arrayName[parseInt(Math.floor(Math.random()*5))] + " " + arrayCognome[parseInt(Math.floor(Math.random()*5))] + "</li>"  
 }
