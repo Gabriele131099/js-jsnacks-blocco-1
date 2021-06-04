a8214019bbfa0e01b0043c2be5e1c48b299a0e01
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-04 16:51:29 +0200

    struttura

--- js snack 12/script.js
+++ js snack 12/script.js
@@ -1,5 +1,5 @@
 var arrayName = ["Pippo" , "Paperino" , "Topolino", "Minnie" , "Pluto" , "Gabriele"]
 var arrayCognome = ["Sanna" , "Casu" , "Aguilera" , "Patermo" , "Riccioli" , "D'Agata"]
-document.getElementById("stampa").innerHTML += "</br>" + arrayName[parseInt(Math.floor(Math.random()*5))] + " " + arrayCognome[parseInt(Math.floor(Math.random()*5))]
-document.getElementById("stampa").innerHTML += "</br>" + arrayName[parseInt(Math.floor(Math.random()*5))] + " " + arrayCognome[parseInt(Math.floor(Math.random()*5))]
-document.getElementById("stampa").innerHTML += "</br>" + arrayName[parseInt(Math.floor(Math.random()*5))] + " " + arrayCognome[parseInt(Math.floor(Math.random()*5))]
+document.getElementById("stampa").innerHTML += "<li>" + arrayName[parseInt(Math.floor(Math.random()*5))] + " " + arrayCognome[parseInt(Math.floor(Math.random()*5))] + "</li>"
+document.getElementById("stampa").innerHTML += "<li>" + arrayName[parseInt(Math.floor(Math.random()*5))] + " " + arrayCognome[parseInt(Math.floor(Math.random()*5))] + "</li>"
+document.getElementById("stampa").innerHTML += "<li>" + arrayName[parseInt(Math.floor(Math.random()*5))] + " " + arrayCognome[parseInt(Math.floor(Math.random()*5))] + "</li>"
